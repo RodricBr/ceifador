53a9d42b70c8ff31b81055f9ecde7902de2bab70
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-11-01 18:41:24 -0300

    Create ceifador.sh

---
+++ ceifador.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+if [[ -z "$*" ]]; then
+  echo -e "Uso: $0 <URL>"
+elif [[ ! -z "$*" ]]; then
+  
+  HORA="$(date +'%d/%m/%y | %T')"
+  
+  touch url
+  echo -e "\n---[ + ]--- Omnisint:\n" >> url
+  OMN=$(curl "https://sonar.omnisint.io/subdomains/$1" -s -k | grep -oE "[a-zA-Z0-9._-]+\.$1" >> url)
+  echo -e "\n---[ + ]--- Anubis:\n" >> url
+  ANU=$(curl "https://jldc.me/anubis/subdomains/$1" -s -k | grep -oE "[a-zA-Z0-9._-]+\.$1" >> url)
+  echo -e "\n+--------------- Scan terminado $HORA ---------------+\n" >> url
+
+  STATUS() {
+      url="$1"
+      urlstatus=$(curl -o /dev/null -k -s --head --write-out  '%{http_code}' "${url}" --max-time 5 ) &&
+      echo "URL: $url CODE: $urlstatus"
+  }
+  export -f STATUS
+
+  parallel -j200 STATUS :::: url >> resp 2>/dev/null
+
+  if [[ -f "url" ]]; then
+    echo -e "Arquivo com subdominios criado!"
+  else
+    echo -e "Nenhuma informação pode ser obtida!"
+  fi
+else
+  false
+fi
